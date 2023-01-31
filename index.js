7418c2d19751b380dd8bc17241b46b6c913f7f26
klimstepuchev <93961658+klimstepuchev@users.noreply.github.com>
2023-01-31 12:02:28 +0300

    Add files

---
+++ index.js
@@ -0,0 +1,14 @@
+const inputANode = document.querySelector(".js-input-a")
+const inputBNode = document.querySelector(".js-input-b")
+const selectOperations = document.querySelector(".js-select-operations")
+const btnResultNode = document.querySelector(".js-btn-result")
+const outputNode = document.querySelector(".js-output")
+
+btnResultNode.addEventListener("click", () => {
+    const a = Number(inputANode.value)
+    const b = Number(inputBNode.value)
+    const operation = selectOperations.value
+
+    const result = calculate(a, b, operation)
+    outputNode.innerHTML = result
+})
